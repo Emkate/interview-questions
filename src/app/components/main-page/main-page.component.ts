923afb7f7cea5d976cf5d6ddc801b88423911e6d
Emkate <tkaczykmichal94@gmail.com>
2021-03-30 13:22:39 +0200

    filters on mobile

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -7,6 +7,7 @@ import { QuestionsService } from 'src/app/state/questions/questions.service';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+  isFiltersVisible = false;
 
   constructor(
     private questionsService: QuestionsService
@@ -15,4 +16,24 @@ export class MainPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleFilters(): void {
+    this.isFiltersVisible = !this.isFiltersVisible;
+    this.setBodyOverflow();
+  }
+
+  showFilters(): void {
+    this.isFiltersVisible = true;
+    this.setBodyOverflow();
+  }
+
+  hideFilters(): void {
+    this.isFiltersVisible = false;
+    this.setBodyOverflow();
+  }
+
+  setBodyOverflow(): void {
+    this.isFiltersVisible ?
+      document.body.classList.add('overflow-hidden') :
+      document.body.classList.remove('overflow-hidden');
+  }
 }
